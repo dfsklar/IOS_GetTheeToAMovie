8fe519531f747a4464ca77498ae02d1e44eae339
David Sklar <sklard@tumblr-MacBookPro-f02af3.local>
2015-08-26 19:43:26 -0400

    The table-view delegate functions are now being called

--- GetTheeToAMovie/GetTheeToAMovie/CatalogViewController.swift
+++ GetTheeToAMovie/GetTheeToAMovie/CatalogViewController.swift
@@ -8,20 +8,22 @@
 
 import UIKit
 
-class CatalogViewController: UIViewController {
+class CatalogViewController: UIViewController, UITableViewDataSource {
     
     let igClientID = "d2f5486933b342db801303d699c76ecb"
     
     var photoList : NSArray = []
-
-    @IBOutlet weak var movieListTable: UITableView!
     
     @IBOutlet weak var prototypeMovieCard: UITableViewCell!
     
+    @IBOutlet weak var catalogTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        self.catalogTable.dataSource = self
+        //self.catalogTable.delegate = self
+
         // Do any additional setup after loading the view, typically from a nib.
         println("catalog view did load")
         var url = NSURL(string: "https://api.instagram.com/v1/media/popular?client_id=\(igClientID)")!
@@ -30,9 +32,24 @@ class CatalogViewController: UIViewController {
             (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
             var responseDict = NSJSONSerialization.JSONObjectWithData(data, options:nil, error:nil) as! NSDictionary
             self.photoList = responseDict["data"] as! NSArray
-            self.movieListTable.reloadData()
+            println("Loaded good")
+            self.catalogTable.reloadData()
         }
     }
+    
+    func tableView(catalogTable: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = catalogTable.dequeueReusableCellWithIdentifier("com.codepath.DemoPrototypeCell", forIndexPath: indexPath) as! UITableViewCell
+        //let cityState = data[indexPath.row].componentsSeparatedByString(", ")
+        //cell.cityLabel.text = cityState.first
+        //cell.stateLabel.text = cityState.last
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.photoList.count
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
