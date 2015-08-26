90067a954bbe71f225357851cc98513626bcc1c0
David Sklar <sklard@UNKNOWN-10-75-141-X.yahoo.com>
2015-08-26 18:59:35 -0400

    async working

--- GetTheeToAMovie/GetTheeToAMovie/CatalogViewController.swift
+++ GetTheeToAMovie/GetTheeToAMovie/CatalogViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class CatalogViewController: UIViewController {
+    
+    let igClientID = "d2f5486933b342db801303d699c76ecb"
+    
+    var photoList : NSArray = []
 
     @IBOutlet weak var movieListTable: UITableView!
     
@@ -17,8 +21,17 @@ class CatalogViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
         println("catalog view did load")
+        var url = NSURL(string: "https://api.instagram.com/v1/media/popular?client_id=\(igClientID)")!
+        var request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {
+            (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            var responseDict = NSJSONSerialization.JSONObjectWithData(data, options:nil, error:nil) as! NSDictionary
+            self.photoList = responseDict["data"] as! NSArray
+            self.movieListTable.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
